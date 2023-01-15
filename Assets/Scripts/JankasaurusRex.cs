60a26fb533af264fb274df1eb2c7e8ad08e101c3
Emily Goose <EmilyGoose@users.noreply.github.com>
2023-01-15 07:10:10 -0500

    most of it works???? wtf

--- Assets/Scripts/JankasaurusRex.cs
+++ Assets/Scripts/JankasaurusRex.cs
@@ -34,27 +34,21 @@ public class JankasaurusRex : MonoBehaviour
         timerMesh = timerDisplay.GetComponent<TextMeshProUGUI>();
         boxButtonScript = boxButton.GetComponent<BoxButtonScript>();
         witHandlerScript = witHandler.GetComponent<WitHandler>();
-        
-        witHandler.SetActive(true);
     }
 
     // Update is called once per frame
     private float lastEvent = 0F;
     private float targetTimeDelta = 0F;
 
+    private float questionStart = 0F;
+    
     void Update()
     {
         float currentTime = Time.time;
         float timeDelta = currentTime - lastEvent;
-        if (true)
-        {
-            textMesh.SetText(witHandlerScript.fullTranscript);
-            timerMesh.SetText(witHandlerScript.tempTranscript);
-        }
-        
 
         // This is how we handle sleep :sunglasses:
-        else if (timeDelta >= targetTimeDelta)
+        if (timeDelta >= targetTimeDelta)
         {
             // THIS IS TERRIBLE CODE
             // DO NOT READ ANY FURTHER
@@ -101,10 +95,34 @@ public class JankasaurusRex : MonoBehaviour
             }
             else if (gameState == 3)
             {
+                targetTimeDelta = 0.5F;
                 textMesh.SetText("Now, tell me about yourself. You have 30 seconds.");
-                targetTimeDelta = 30F;
+                questionStart = currentTime + 5;
+                targetTimeDelta = 5F;
                 gameState += 1;
+            } else if (gameState == 4)
+            {
+                gameState += 1;
+                witHandler.SetActive(true);
             }
+            else if (gameState == 5)
+            {
+                if (currentTime - questionStart >= 30)
+                {
+                    gameState += 1;
+                }
+                else
+                {
+                    timerMesh.SetText((30 - (currentTime - questionStart)).ToString(CultureInfo.CurrentCulture));
+                    textMesh.SetText(witHandlerScript.fullTranscript + " " + witHandlerScript.tempTranscript);
+                    
+                }
+            } else if (gameState == 6)
+            {
+                targetTimeDelta = 0.5F;
+                witHandler.SetActive(false);
+                gameState += 1;
+            } 
             else
             {
                 textMesh.SetText("no game state :(\nWe haven't made this part yet...");
